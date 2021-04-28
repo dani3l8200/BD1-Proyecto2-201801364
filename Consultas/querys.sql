b8a74c1861fc9adebed23ebf8c38e91e5327e10d
Juan Daniel <dani3l25948@gmail.com>
2021-04-28 21:46:32 +0000

    Falta consulta 4 y 16

--- Consultas/querys.sql
+++ Consultas/querys.sql
@@ -12,13 +12,15 @@ left join pais_respuesta r on p.id_pais = r.id_pais
 left join region re on p.id_region = re.id_region 
 group by p.id_pais 
 order by  pais asc;
+select * from inventor;
 
 /*Consulta #3*/
-select p.pais, p.area_km2 from pais p 
+select distinct p.pais, p.area_km2 from pais p 
 left join inventor i on p.id_pais = i.id_pais
 left join frontera f on p.id_pais = f.id_pais
-where i.id_pais is null and f.id_pais is null
-order by area_km2 desc;
+left join pais pp on f.check_frontera = pp.id_pais
+where i.id_pais is null and pp.id_pais is null
+order by p.area_km2 desc;
 
 /*Consulta #5*/
 create or replace view promedios as select * from
@@ -74,3 +76,63 @@ inner join invento i on imag.id_invento = i.id_invento
 where i.anio_invento like '18%' and (inv.nombre like 'B%r' or inv.nombre like 'B%n'); 
 
 /*Consulta 11*/
+select p.pais, p.area_km2, count(check_frontera) num_front
+from frontera f 
+inner join pais p on f.id_pais = p.id_pais
+group by f.id_pais 
+having count(check_frontera) > 7 
+order by p.area_km2 desc;
+
+/*Consulta 12*/
+select i.nombre invento from invento i
+where i.nombre like 'L___';
+
+/*Consulta 13*/
+select nombre, salario, comision, (salario + comision) total 
+from profesional 
+where comision > (0.25*salario);
+
+/*Consulta 14*/
+SELECT e.encuesta, count(pr.id_pais) resp_pais
+from pais_respuesta pr
+inner join respuesta r on pr.id_respuesta = r.id_respuesta 
+inner join pregunta p on r.id_pregunta = p.id_pregunta
+inner join encuesta e on p.id_encuesta = e.id_encuesta
+group by e.id_encuesta;
+
+/*Consulta 15*/
+select p.pais, p.poblacion from pais p 
+where p.poblacion > (
+	select sum(poblacion) from pais p 
+    inner join region r on p.id_region = r.id_region and r.region = 'Centro America'
+);
+
+/*Consulta 16*/
+
+/*Consulta 17*/
+select nombre, anio_invento from invento 
+where anio_invento = (
+	select ii.anio_invento from inventado i 
+    inner join inventor inv on i.id_inventor = inv.id_inventor
+    inner join invento ii on i.id_invento = ii.id_invento
+    where inv.nombre = 'Benz'
+);
+
+/*Consulta 18*/
+select p.pais, p.poblacion from pais p
+left join frontera f on p.id_pais = f.id_pais 
+where f.check_frontera is null and 
+p.area_km2 >= (
+	select area_km2 from pais
+    where pais = 'Japon'
+);
+
+/*Consulta 19*/
+select distinct p.pais, pp.pais frontera from frontera f
+inner join pais p on f.id_pais = p.id_pais
+inner join pais pp on f.check_frontera = pp.id_pais;
+
+/*Consulta 20*/
+select nombre, salario, comision 
+from profesional 
+where salario > (2*comision);
